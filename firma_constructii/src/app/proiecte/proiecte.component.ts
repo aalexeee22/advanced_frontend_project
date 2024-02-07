d012fe7b0a4909facd652e777d6f0f538901e186
aalexeee22 <alex2aalexeee@gmail.com>
2024-02-08 00:59:35 +0200

    componenta_reutilizabila

--- firma_constructii/src/app/proiecte/proiecte.component.ts
+++ firma_constructii/src/app/proiecte/proiecte.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Project } from '../project.model';
 @Component({
   selector: 'app-proiecte',
   templateUrl: './proiecte.component.html',
   styleUrl: './proiecte.component.scss'
 })
 export class ProiecteComponent {
-
+  constructor() {
+    this.project = {} as Project; 
+  }
+  @Input() project: Project; // Input pentru a primi detalii despre proiect din componenta părinte
 }
--- firma_constructii/src/app/rrs/rrs.component.ts
+++ firma_constructii/src/app/rrs/rrs.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Project } from '../project.model';
 @Component({
   selector: 'app-rrs',
   templateUrl: './rrs.component.html',
   styleUrl: './rrs.component.scss'
 })
 export class RrsComponent {
-
+  projects: Project[] = [
+    {
+      title: '1.  Utilaje pentru lucrări mici sau mari',
+      description: 'SCHAEFF HR 16',
+      imageUrl: 'https://github.com/aalexeee22/Proiect-tehnici-web/blob/main/imagini/schaeff_hr_16.jpeg?raw=true'
+    },
+    {
+      title: '2. Utilaje pentru deszăpeziri',
+      description: 'Oricând la nevoie!',
+      imageUrl: 'https://github.com/aalexeee22/Proiect-tehnici-web/blob/main/imagini/utilaje.jpeg?raw=true'
+    },
+    {
+      title: '3. Lucrări pentru firme',
+      description: 'Lisa Dräxlmaier',
+      imageUrl: 'https://raw.githubusercontent.com/aalexeee22/Proiect-tehnici-web/main/imagini/draxlmaier.jpeg'
+    }  
+  ];
 }
